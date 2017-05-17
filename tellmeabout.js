db2d19e2186d351cbb74cdadcb0059aca40dc3df
jonathanwshapiro <jonathanwshapiro@gmail.com>
2017-05-16 21:32:49 -0400

    Update tellmeabout.js

--- tellmeabout.js
+++ tellmeabout.js
@@ -1,3 +1,48 @@
-exports.run = (api, event) => {
-	api.sendMessage("hello world", event.thread_id);
+const topic = ["coop",
+	       "plagueis",
+	       "shane",
+	       "snakes",
+	       "veganism"],
+    information = ["who?",
+		   "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.",
+		   "a hero, taken before his time",
+		   "We're the TUNNEL SNAKES. That's US. And we RULE.",
+		   "just don't take your advice from Jon"];
+
+// The main slapper
+exports.tellMe = function(topic){
+    var topicList = [
+	//topics
+	"coop",
+	"plagueis",
+	"shane",
+	"snakes",
+	"veganism"
+	];
+	//information
+    var infoList = [
+	"who?",
+	"Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.",
+	"a hero, taken before his time",
+	"We're the TUNNEL SNAKES. That's US. And we RULE.",
+	"just don't take your advice from Jon"
+	];
+	
+    var index = topiclist.indexOf(topic);
+    if(index < 0){
+        return "Why the fuck would I know anything about that";
+    } else {
+        return infolist[index];
+};
+
+// Make the slapper work for it's money
+exports.run = function(api, event) {
+    // Obtain the query
+    var query = event.arguments_body;
+
+    // get the feels
+    var result = exports.tellMe(query);
+
+    // Send to facebook
+    api.sendMessage(result, event.thread_id);
 };
