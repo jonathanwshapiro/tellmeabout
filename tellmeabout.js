cc406f3e36fe8c44e4e90cb0a2f1932a705a20b0
jonathanwshapiro <jonathanwshapiro@gmail.com>
2017-05-16 21:35:04 -0400

    Update tellmeabout.js

--- tellmeabout.js
+++ tellmeabout.js
@@ -1,14 +1,3 @@
-const topic = ["coop",
-	       "plagueis",
-	       "shane",
-	       "snakes",
-	       "veganism"],
-    information = ["who?",
-		   "Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.",
-		   "a hero, taken before his time",
-		   "We're the TUNNEL SNAKES. That's US. And we RULE.",
-		   "just don't take your advice from Jon"];
-
 // The main slapper
 exports.tellMe = function(topic){
     var topicList = [
@@ -25,14 +14,14 @@ exports.tellMe = function(topic){
 	"Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It’s not a story the Jedi would tell you. It’s a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.",
 	"a hero, taken before his time",
 	"We're the TUNNEL SNAKES. That's US. And we RULE.",
-	"just don't take your advice from Jon"
+	"Just don't take your advice from Jon"
 	];
 	
-    var index = topiclist.indexOf(topic);
+    var index = topicList.indexOf(topic);
     if(index < 0){
         return "Why the fuck would I know anything about that";
     } else {
-        return infolist[index];
+        return infoList[index];
 };
 
 // Make the slapper work for it's money
